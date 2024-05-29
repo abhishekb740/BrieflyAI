762f91312266ff006ef403cc36c730f098fdf45d
Abhishek <abhishekb740@gmail.com>
2024-05-29 17:10:01 +0530

    style: Home page improvements

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -24,28 +24,27 @@ export default function Hero() {
     return (
         <main className="flex min-h-screen flex-col items-center p-20 gap-16">
             <div className="flex flex-col gap-8 justify-center items-center" >
-                <div className="text-2xl md:text-3xl font-title">
+                <div className="text-2xl md:text-3xl font-title font-bold">
                     VortexAI
                 </div>
-                <div className="text-md md:text-lg">
-                    Shaping the Future of Recruitment 🤖
+                <div className="text-md font md:text-xl">
+                    Shaping the Future of Recruitment with AI 🤖
                 </div>
             </div>
-            <div className="flex flex-col gap-4 justify-center items-center">
+            <div className="flex flex-col gap-16 justify-center items-center">
                 <div className="text-lg md:text-2xl">
                     Please choose your topic for the Interview
                 </div>
                 <div>
-                    <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-20">
                         {Topics.map((topic, index) => (
-                            <div key={index} className="bg-zinc-800 p-2 rounded-lg">
+                            <div key={index} className="bg-zinc-800 p-2 rounded-lg flex justify-center text-lime-500 hover:cursor-pointer">
                                 {topic.name}
                             </div>
                         ))} 
                     </div>
                 </div>
             </div>
-
         </main>
     )
 }

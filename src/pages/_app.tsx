13dc6867ef64d6834a712813cd1e3fefc62c4501
Vaibhav <vaibhav.p@xpuls.ai>
2024-06-02 05:55:05 +0530

    feat: add email auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { HuddleClient, HuddleProvider } from "@huddle01/react";
+  
 
 const huddleClient = new HuddleClient({
   projectId: "zMQHa6hH5hGrxfwYZp7z8I-1lWScI7UA",
@@ -14,8 +15,8 @@ const huddleClient = new HuddleClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <HuddleProvider client={huddleClient}>
-      <Component {...pageProps} />
-    </HuddleProvider>
+      <HuddleProvider client={huddleClient}>
+        <Component {...pageProps} />
+      </HuddleProvider>
   );
 }
